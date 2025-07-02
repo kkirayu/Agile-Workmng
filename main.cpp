44b16639935115551ca7dbfff3f5ce414e63b19a
kkirayu <kukicha76@gmail.com>
2025-07-02 19:09:30 +0700

    comment removed

--- main.cpp
+++ main.cpp
@@ -2,17 +2,14 @@
 #include <iostream>
 
 int main() {
-    // Inisialisasi pengguna admin default
     UserNode* adminUser = new UserNode();
     adminUser->data.username = "admin";
     adminUser->data.password = "admin123";
     adminUser->next = headUser;
     headUser = adminUser;
 
-    // Tampilkan menu utama untuk memulai interaksi pengguna
     tampilkanMainMenu();
 
-    // Bersihkan semua memori yang dialokasikan sebelum program berakhir
     bersihkanMemori();
 
     cout << "Terima kasih telah menggunakan program ini!\n";
